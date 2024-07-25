1f9438c5454bcf401a577bf2c0ef269540df2c79
Avirajsinh <avirajsinh0010@gmail.com>
2024-07-25 16:45:58 +0530

    1.0.16

--- api/app.py
+++ api/app.py
@@ -5,17 +5,14 @@ import numpy as np
 from PIL import Image
 import io
 import os
+import psutil
 import logging
 
 app = Flask(__name__)
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-
 # Load your trained model
 model_path = os.path.join(os.path.dirname(__file__), 'model', 'final5.h5')
 model = load_model(model_path)
-logging.info("Model loaded successfully.")
 
 def preprocess_image(image):
     image = image.resize((224, 224))  # Adjust the size to match your model input size
@@ -25,27 +22,34 @@ def preprocess_image(image):
     image = np.expand_dims(image, axis=0)
     return image
 
+def log_memory_usage():
+    process = psutil.Process(os.getpid())
+    memory_info = process.memory_info()
+    app.logger.info(f"Memory Usage: RSS={memory_info.rss / 1024 ** 2:.2f} MB")
+
 @app.route('/api/predict', methods=['POST'])
 def predict():
+    log_memory_usage()
     if 'file' not in request.files:
-        logging.error("No file part in the request.")
+        app.logger.error("No file part")
         return jsonify({'error': 'No file part'})
     file = request.files['file']
     if file.filename == '':
-        logging.error("No selected file.")
+        app.logger.error("No selected file")
         return jsonify({'error': 'No selected file'})
     if file:
         try:
             image = Image.open(io.BytesIO(file.read()))
-            logging.info("Image received, processing...")
+            app.logger.info("Image received, processing...")
             processed_image = preprocess_image(image)
-            logging.info("Image processed, making prediction...")
+            app.logger.info("Image processed, making prediction...")
             prediction = model.predict(processed_image)
             result = np.argmax(prediction, axis=1)[0]
-            logging.info(f"Prediction result: {result}")
+            app.logger.info(f"Prediction result: {result}")
+            log_memory_usage()
             return jsonify({'prediction': int(result)})
         except Exception as e:
-            logging.error(f"Error during prediction: {e}")
+            app.logger.error(f"Error: {e}")
             return jsonify({'error': str(e)})
 
 @app.route('/api/test', methods=['GET'])
