e5c884c32b28c8456fac8643579fac8a67370fa9
Avirajsinh <avirajsinh0010@gmail.com>
2024-07-25 15:11:51 +0530

    1.0.9

--- api/app.py
+++ api/app.py
@@ -40,4 +40,4 @@ def test():
     return jsonify({'message': 'API is working'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True,host='0.0.0.0', port=5000)
